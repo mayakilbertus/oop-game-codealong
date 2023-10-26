d9e54d7a2665afe64490028d080273a55385f0a3
mayakilbertus <maya.kilbertus@yahoo.de>
2023-10-26 17:35:15 +0200

    thursday changes

--- js/main.js
+++ js/main.js
@@ -1,1 +1,83 @@
-console.log("js loaded....")
+class Player {
+  constructor() {
+    // initialize properties
+    this.positionX = 50;
+    this.positionY = 0;
+    this.height = 10;
+    this.width = 20;
+
+    // dom manipulation to reflect initial values (size, position)
+    this.playerElm = document.getElementById("player");
+    this.playerElm.style.width = this.width + "vw";
+    this.playerElm.style.height = this.height + "vh";
+    this.playerElm.style.left = this.positionX + "vw";
+    this.playerElm.style.bottom = this.positionY + "vh";
+  }
+  moveLeft() {
+    this.positionX--;
+    this.playerElm.style.left = this.positionX + "vw";
+  }
+  moveRight() {
+    this.positionX++;
+    this.playerElm.style.left = this.positionX + "vw";
+  }
+}
+
+class Obstacle {
+  constructor() {
+    this.positionX = 50;
+    this.positionY = 100;
+    this.height = 10;
+    this.width = 30;
+    this.obstacleElm = null;
+
+    this.createDomElement();
+  }
+  createDomElement() {
+    // step1: create the element
+    this.obstacleElm = document.createElement("div");
+
+    // step2: add content or modify
+    this.obstacleElm.classList.add("obstacle");
+    this.obstacleElm.style.width = this.width + "vw";
+    this.obstacleElm.style.height = this.height + "vh";
+    this.obstacleElm.style.left = this.positionX + "vw";
+    this.obstacleElm.style.bottom = this.positionY + "vh";
+
+    // step3: append to the dom: `parentElm.appendChild()`
+    const parentElm = document.getElementById("board");
+    parentElm.appendChild(this.obstacleElm);
+  }
+  moveDown() {
+    this.positionY--;
+    this.obstacleElm.style.bottom = this.positionY + "vh";
+  }
+}
+
+const player = new Player();
+
+const obstaclesArr = []; // will store instances of the class Obstacle
+
+// create obstacles
+setInterval(() => {
+  const newObstacle = new Obstacle();
+  obstaclesArr.push(newObstacle);
+}, 2000);
+
+// move all obstacles
+setInterval(() => {
+  obstaclesArr.forEach((obstacleInstance) => {
+    obstacleInstance.moveDown();
+  });
+}, 50);
+
+document.addEventListener("keydown", (e) => {
+  switch (e.code) {
+    case "ArrowLeft":
+      player.moveLeft();
+      break;
+    case "ArrowRight":
+      player.moveRight();
+      break;
+  }
+});
